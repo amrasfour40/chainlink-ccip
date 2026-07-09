52f1276d18eccf1e9514bfd9c5c1aacbdcbfc659
amrasfour40 <amrasfour1987@gmail.com>
2026-07-09 20:31:27 +0000

    Fix Ownable import in MyOFT.sol and MyOFTAdapter.sol - both now compile cleanly, Code 1 and Code 2 tests confirmed passing together

--- mini-ccip/src/MyOFT.sol
+++ mini-ccip/src/MyOFT.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 import { OFT } from "@layerzerolabs/oapp-evm/contracts/oft/OFT.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 contract MyOFT is OFT {
     constructor(string memory _name, string memory _symbol, address _lzEndpoint, address _delegate)
         OFT(_name, _symbol, _lzEndpoint, _delegate)
--- mini-ccip/src/MyOFTAdapter.sol
+++ mini-ccip/src/MyOFTAdapter.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 import { OFTAdapter } from "@layerzerolabs/oapp-evm/contracts/oft/OFTAdapter.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 contract MyOFTAdapter is OFTAdapter {
     constructor(address _token, address _lzEndpoint, address _delegate)
         OFTAdapter(_token, _lzEndpoint, _delegate)
